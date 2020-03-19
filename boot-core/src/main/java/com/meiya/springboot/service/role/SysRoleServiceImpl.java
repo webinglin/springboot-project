138a81494770e2974639068abad06db48a5876e8
webinglin@163.com <webinglin@163.com>
2020-03-19 21:50:27 +0800

    测试

--- boot-core/src/main/java/com/meiya/springboot/service/role/SysRoleServiceImpl.java
+++ boot-core/src/main/java/com/meiya/springboot/service/role/SysRoleServiceImpl.java
@@ -10,4 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class SysRoleServiceImpl implements SysRoleService {
+
+
 }
